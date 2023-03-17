44c4a801e6875411f220ffe00c11a3719fe1216e
LeonhardRuegg <57802897+LeonhardRuegg@users.noreply.github.com>
2023-03-17 09:03:33 +0100

    Updates

--- main.py
+++ main.py
@@ -30,25 +30,33 @@ def emailFormats(firstName, lastName, domain):
 
 def EmailVerify(emails, domain):
 
-    # MX record lookup
-    records = dns.resolver.resolve(domain, 'MX')
-    mxRecord = records[0].exchange
-    mxRecord = str(mxRecord)
+    try:
+        # MX record lookup
+        records = dns.resolver.resolve(domain, 'MX')
+        mxRecord = records[0].exchange
+        mxRecord = str(mxRecord)
 
-    # This email will be used to "send" an email to the server
-    fromAddress = 'just_a_place_holder@domain.com'
+        # This email will be used to "send" an email to the server
+        fromAddress = 'just_a_place_holder@domain.com'
 
-    # This email address will be used to check if the server is accept all
-    # If this obviously invalid email is accepted, the server is accept all
-    acceptall = "invalid.email.9238381" + "@" + domain
+        # This email address will be used to check if the server is accept all
+        # If this obviously invalid email is accepted, the server is accept all
+        acceptall = "invalid.email.9238381" + "@" + domain
 
-    # Connect to the server and verify the email address
-    server = smtplib.SMTP()
-    server.connect(mxRecord)
-    server.helo(server.local_hostname)
-    server.mail(fromAddress)
+        # Connect to the server and verify the email address
+        server = smtplib.SMTP()
+        server.connect(mxRecord)
+        server.helo(server.local_hostname)
+        server.mail(fromAddress)
+
+
+    except:
+        emailfound = True
+        message = "- Invalid Domain - "
+        email = "Invalid Domain"
+        return email, str(message), None
+
     code, message = server.rcpt(str(acceptall))
-
     emailfound = False
 
     if code == 250:
@@ -86,60 +94,77 @@ emailonly = st.checkbox('Email Format Only', value=False, help="Only constructs 
 # Set the button and start all the functions when clicked
 if st.button("Submit", use_container_width=True, type="primary"):
 
-    # Extract the domain from webaddress
-    domain = tldextract.extract(domain)
-    domain = domain.domain + "." + domain.suffix
+    error = False
 
-    # Split first and lastName if both habe been copied into firstName field
-    if " " in firstName:
-        namesplit = firstName.split()
-        firstName = namesplit[0]
-        lastName = namesplit[1]
+    if len(firstName)<1:
+        st.error("First Name Missing")
+        error = True
 
-    firstName = firstName.lower()
-    lastName = lastName.lower()
+    if len(lastName)<1:
+        st.error("Last Name Missing")
+        error = True
 
-    # If a mail address is copied into the firstName field, verify this address
-    # If not, generate the different email formats and verify those
-    if "@" in firstName:
-        emails = [firstName]
-        domain = tldextract.extract(firstName)
+    if len(domain)<1:
+        st.error("Domain Missing")
+        error = True
+
+
+    if error == False:
+
+        # Extract the domain from webaddress
+        domain = tldextract.extract(domain)
         domain = domain.domain + "." + domain.suffix
-    else:
-        emails = emailFormats(unidecode(firstName), unidecode(lastName), domain)
 
-    # Check for Umlaute
-    umlautMode = False
-    umlaute = ['ä', 'ü', 'ö']
-    umlautName = firstName + lastName
+        # Split first and lastName if both have been copied into firstName field
+        if " " in firstName:
+            namesplit = firstName.split()
+            firstName = namesplit[0]
+            lastName = namesplit[1]
 
-    for umlaut in umlaute:
-        if umlaut in umlautName:
-            umlautMode = True
+        firstName = firstName.lower()
+        lastName = lastName.lower()
 
-    # If and Umlaut is detected, both email format variants are generated and verified
-    # Eg. leonhard.ruegg@zutt.ch and also leonhard.rueegg@zutt.ch and so forth
-    if umlautMode == True:
-        vowel_char_map = {ord('ä'): 'ae', ord('ü'): 'ue', ord('ö'): 'oe'}
-        firstName = firstName.translate(vowel_char_map)
-        lastName = lastName.translate(vowel_char_map)
-        firstName = unidecode(firstName)
-        lastName = unidecode(lastName)
-        umlautmails = emailFormats(firstName, lastName, domain)
-        emails.extend(umlautmails)
+        # If a mail address is copied into the firstName field, verify this address
+        # If not, generate the different email formats and verify those
+        if "@" in firstName:
+            emails = [firstName]
+            domain = tldextract.extract(firstName)
+            domain = domain.domain + "." + domain.suffix
+        else:
+            emails = emailFormats(unidecode(firstName), unidecode(lastName), domain)
+
+        # Check for Umlaute
         umlautMode = False
+        umlaute = ['ä', 'ü', 'ö']
+        umlautName = firstName + lastName
 
-    # Verify the emails
-    if emailonly == False:
-        email, message, help = EmailVerify(emails, domain)
-    else:
-        email = emails[0]
-        message = "Email Format Only"
-        help = None
+        for umlaut in umlaute:
+            if umlaut in umlautName:
+                umlautMode = True
 
-    # Display the valid email
-    with st.container():
-        st.metric(label="", value=email, delta=message, help=help)
+        # If and Umlaut is detected, both email format variants are generated and verified
+        # Eg. leonhard.ruegg@zutt.ch and also leonhard.rueegg@zutt.ch and so forth
+        if umlautMode == True:
+            vowel_char_map = {ord('ä'): 'ae', ord('ü'): 'ue', ord('ö'): 'oe'}
+            firstName = firstName.translate(vowel_char_map)
+            lastName = lastName.translate(vowel_char_map)
+            firstName = unidecode(firstName)
+            lastName = unidecode(lastName)
+            umlautmails = emailFormats(firstName, lastName, domain)
+            emails = umlautmails + emails
+            umlautMode = False
+
+        # Verify the emails
+        if emailonly == False:
+            email, message, help = EmailVerify(emails, domain)
+        else:
+            email = emails[0]
+            message = "Email Format Only"
+            help = None
+
+        # Display the valid email
+        with st.container():
+            st.metric(label="", value=email, delta=message, help=help)
 
 # Allow the enter button to be used for submit
 components.html(
